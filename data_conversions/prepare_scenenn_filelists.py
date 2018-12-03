7c4c83b404d438e9be637ef883aa438fb0d506c6
Thomas Schattschneider <tschattschneider@mailbox.org>
2018-12-03 14:59:16 +0100

    Clean up code

--- data_conversions/prepare_scenenn_filelists.py
+++ data_conversions/prepare_scenenn_filelists.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 from sklearn.model_selection import train_test_split
@@ -10,20 +11,37 @@ def move_to_subdirectory(files, directory):
         file.replace(destination)  # The replace command moves the file
 
 
-root_dir = Path.home() / 'Thesis/data/SceneNN'
-data_dir = root_dir / 'preprocessed'
-train_dir = data_dir / 'train'
-val_dir = data_dir / 'validation'
-test_dir = data_dir / 'test'
+def main():
+    data_dir = Path.home() / 'Thesis/data/SceneNN/preprocessed'
+    train_dir = data_dir / 'train'
+    val_dir = data_dir / 'validation'
+    test_dir = data_dir / 'test'
+    h5files = list(data_dir.glob('*.hdf5'))
 
-h5files = list(data_dir.glob('*.hdf5'))
+    # Split data into subdirectories
+    if any(h5files):
+        # Split data into train, validation and test set (60 / 20 / 20)
+        files_dev, files_test = train_test_split(h5files, test_size=0.2)
+        files_train, files_val = train_test_split(files_dev, test_size=0.25)
 
-# Split data into subdirectories
-if any(h5files):
-    # Split data into train, validation and test set (60 / 20 / 20)
-    files_dev, files_test = train_test_split(h5files, test_size=0.2)
-    files_train, files_val = train_test_split(files_dev, test_size=0.25)
+        # Move the sets to seperate subdirectories.
+        # the replace command moves a file.
+        train_dir.mkdir()
+        for file in files_train:
+            destination = train_dir / file.name
+            file.replace(destination)
 
-    move_to_subdirectory(files_train, train_dir)
-    move_to_subdirectory(files_val, val_dir)
-    move_to_subdirectory(files_test, test_dir)
+        val_dir.mkdir()
+        for file in files_val:
+            destination = val_dir / file.name
+            file.replace(destination)
+
+        test_dir.mkdir()
+        for file in files_test:
+            destination = test_dir / file.name
+            file.replace(destination)
+
+
+
+if __name__ == '__main__':
+    main()
