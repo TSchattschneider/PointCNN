ea357ba5df4980849f0b994b6eb3bc7d5b9fea42
Thomas Schattschneider <tschattschneider@mailbox.org>
2018-12-04 14:11:15 +0100

    Finalize filelist creation for SceneNN.

--- data_conversions/prepare_scenenn_filelists.py
+++ data_conversions/prepare_scenenn_filelists.py
@@ -1,5 +1,4 @@
 import argparse
-import math
 from pathlib import Path
 
 from sklearn.model_selection import train_test_split
@@ -17,13 +16,14 @@ def main():
     train_dir = data_dir / 'train'
     val_dir = data_dir / 'validation'
     test_dir = data_dir / 'test'
+    filelist_dir = data_dir / 'filelists'
     h5files = list(data_dir.glob('*.hdf5'))
 
     # Split data into subdirectories
     if any(h5files):
         # Split data into train, validation and test set (60 / 20 / 20)
-        files_dev, files_test = train_test_split(h5files, test_size=0.2)
-        files_train, files_val = train_test_split(files_dev, test_size=0.25)
+        files_train, files_test = train_test_split(h5files, test_size=0.2)
+        files_train, files_val = train_test_split(files_train, test_size=0.25)
 
         # Move the sets to seperate subdirectories.
         # the replace command moves a file.
@@ -43,16 +43,36 @@ def main():
             file.replace(destination)
 
     # Split list of training files into chunks
-    files_train = list(train_dir.iterdir())
-    n = args.chunk_size
-    chunks_train = [files_train[i:i + n] for i in range(0, len(files_train), n)]
+    if not filelist_dir.exists():
+        files_train = list(train_dir.glob('*.hdf5'))
+        n = args.chunk_size
+        chunks_train = [files_train[i:i + n] for i in range(0, len(files_train), n)]
 
-    # Write lists of chunked files to txt
-    for idx, chunk in enumerate(chunks_train):
-        with Path(data_dir / f'train_chunk_{idx}.txt').open(mode='w') as f:
-            for path in chunk:
-                f.write(f'{path}\n')
+        # Write lists of chunked files to a text file
+        filelist_dir.mkdir()
+        for idx, chunk in enumerate(chunks_train):
+            chunk_textfile = filelist_dir / f'train_chunk_{idx}.txt'
+            with chunk_textfile.open(mode='w') as txt:
+                for filepath in chunk:
+                    txt.write(f'{filepath}\n')
 
+        # Group the chunk text files into a single text file
+        with Path(data_dir / 'train_files.txt').open(mode='w') as txt:
+            chunk_textfiles = list(filelist_dir.glob('*chunk*.txt'))
+            for filepath in chunk_textfiles:
+                txt.write(f'{filepath}\n')
+
+    # Write list of validation files
+    with Path(data_dir / 'validation_files.txt').open(mode='w') as txt:
+        files_val = list(val_dir.glob('*.hdf5'))
+        for filepath in files_val:
+            txt.write(f'{filepath}\n')
+
+    # Write list of test files
+    with Path(data_dir / 'test_files.txt').open(mode='w') as txt:
+        files_test = list(test_dir.glob('*.hdf5'))
+        for filepath in files_test:
+            txt.write(f'{filepath}\n')
 
 if __name__ == '__main__':
     main()
