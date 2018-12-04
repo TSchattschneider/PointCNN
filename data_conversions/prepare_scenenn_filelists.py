0e76f067e91914ff93399479fb46624f9e1d96c3
Thomas Schattschneider <tschattschneider@mailbox.org>
2018-12-04 08:51:20 +0100

    Implement splitting list of training files into (lists of) chunks.

--- data_conversions/prepare_scenenn_filelists.py
+++ data_conversions/prepare_scenenn_filelists.py
@@ -1,17 +1,18 @@
 import argparse
+import math
 from pathlib import Path
 
 from sklearn.model_selection import train_test_split
 
 
-def move_to_subdirectory(files, directory):
-    directory.mkdir()
-    for file in files:
-        destination = directory / file.name
-        file.replace(destination)  # The replace command moves the file
-
-
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--chunk_size',
+                        help='Size of training file chunks, '
+                             'specifies how many h5 files will be loaded at a time.',
+                        type=int, default=8)
+    args = parser.parse_args()
+
     data_dir = Path.home() / 'Thesis/data/SceneNN/preprocessed'
     train_dir = data_dir / 'train'
     val_dir = data_dir / 'validation'
@@ -41,6 +42,16 @@ def main():
             destination = test_dir / file.name
             file.replace(destination)
 
+    # Split list of training files into chunks
+    files_train = list(train_dir.iterdir())
+    n = args.chunk_size
+    chunks_train = [files_train[i:i + n] for i in range(0, len(files_train), n)]
+
+    # Write lists of chunked files to txt
+    for idx, chunk in enumerate(chunks_train):
+        with Path(data_dir / f'train_chunk_{idx}.txt').open(mode='w') as f:
+            for path in chunk:
+                f.write(f'{path}\n')
 
 
 if __name__ == '__main__':
